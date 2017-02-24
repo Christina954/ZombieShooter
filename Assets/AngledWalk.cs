f74930b987354780b7650dfa641bbdf09b3d429b
Christina S. Wang <christina.s.wang@vanderbilt.edu>
2017-02-24 03:26:56 -0600

    Upload from desktop

---
+++ Assets/AngledWalk.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class AngledWalk: MonoBehaviour
+{
+    //VR Main Camera
+    public GameObject Body;
+    public GameObject parentMove;
+    //Angle at which walk/stop will be triggered (x value of camera?)
+    public float toggleAngle = 30.0f;
+    //how fast to move
+    public float speed = 0.01f;
+    //should I move forward or not
+    public bool moveforward;
+    //character controller script
+    //private CharacterController cc;
+    private Rigidbody rb;
+    Vector3 temPos;
+
+    public bool islocal;
+    // Use this for initialization
+    void Start()
+    {
+        //find the character controller
+        //cc = GetComponent<CharacterController>();
+        rb = GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //Check to see if the head has rotated down to the toggleAngle, but not more than straight down
+        if (Body.transform.eulerAngles.x >= toggleAngle && Body.transform.eulerAngles.x < 90.0f)
+        {
+
+            //Debug.Log("Euler Angle: First Loop" + Body.transform.eulerAngles.x);
+            moveforward = true;
+
+        }
+        else
+        {
+            //Debug.Log ("Euler Angle x" + Body.eulerAngles.x);
+            //stop moving
+            moveforward = false;
+        }
+        if (moveforward)
+        {
+            temPos = transform.position; //current coordinates
+            Vector3 tmp = GvrReticle.returnPt();
+            //Debug.Log(tmp);
+            tmp.y = 2.0f;//Maybe set this diferently for the bombs???
+            //tmp = tmp * Time.deltaTime * speed;
+            //parentMove.transform.position = new Vector3(temPos.x - ((tmp.x / 60)), 2.0f, temPos.z - ((tmp.z / 60)));
+            parentMove.transform.position = new Vector3(temPos.x + (tmp.x - temPos.x) / 60, 1.5f, temPos.z + (tmp.z - temPos.z) / 60);
+            //float step = speed * Time.deltaTime;
+            //transform.position = Vector3.MoveTowards(parentMove, tmp, step);
+            //Debug.Log(Body.transform.position);
+
+        }
+    }
+}
---
+++ Assets/StartGame.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement; 
+
+public class StartGame : MonoBehaviour
+{
+    //public GUITexture menu1 = null;
+    //public GUITexture submenu1 = null;
+    //public GUITexture submenu2 = null;
+    //private bool menu1Expanded = false;
+    //public string loadlevel; 
+
+    public void ChangeScene(int Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+
+}
+
+    // Update is called once per frame
+    void Update() {
+        //OnGUI(); 
+    }
+
+    #region IGvrGazeResponder implementation
+
+    /// Called when the user is looking on a GameObject with this script,
+    /// as long as it is set to an appropriate layer (see GvrGaze).
+    public void OnGazeEnter()
+    {
+        Debug.Log("On Gaze Enter");
+    }
+
+    /// Called when the user stops looking on the GameObject, after OnGazeEnter
+    /// was already called.
+    public void OnGazeExit()
+    {
+        Debug.Log("On Gaze Exit");
+    }
+
+    /// Called when the viewer's trigger is used, between OnGazeEnter and OnGazeExit.
+    public void OnGazeTrigger()
+    {
+        Debug.Log("On Gaze Trigger");
+        //Debug.Log(NetworkManager.singleton);
+        //NetworkManager.singleton.matchMaker.CreateMatch("match", 4, true, "", "", "", 0, 0, OnMatchCreate);
+    }
+
+    #endregion
+    /*
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(loadlevel);
+        /*
+        if (GUI.Button(new Rect(10, 10, 120, 50), "menu1"))
+        {
+            menu1Expanded = !menu1Expanded;
+            // do you menu1 stuff
+        }
+
+        if (menu1Expanded)
+        {
+            if (GUI.Button(new Rect(10, 60, 50, 50), "submenu1"))
+            {
+
+                // do you submenu1 stuff
+            }
+
+            if (GUI.Button(new Rect(60, 60, 50, 50), "submenu2"))
+            {
+
+                // do you submenu2 stuff
+            }
+        } */
+        
+    } 
+
+
